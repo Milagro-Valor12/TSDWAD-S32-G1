73766b89945ec497bf296aa7b27943e0b253f85e
jesicaeamaya <106560577+jesicaeamaya@users.noreply.github.com>
2022-08-14 15:56:37 -0300

    Add files via upload

---
+++ jesyamaya.py
@@ -0,0 +1,15 @@
+#crear una lista de python denominada dueño2 y recorrerla con un bucle mostrando sus elementos
+#por pantalla con excepcion del DNI y apellido. los elementos de la lista son:
+#23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"
+
+Dueño2 = [23546987,  "perez",  "Maria", 4789689,  "Pueyrredon  811"]
+
+for i in Dueño2:
+
+    print(Dueño2[2],Dueño2[3],Dueño2[4])
+
+
+
+for x in range(2,len(Dueño2)):
+    print (Dueño2[x])
+
